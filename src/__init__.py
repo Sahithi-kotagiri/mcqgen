7672f39e346ca2ea17ee11cee15b498780485697
Sahithi-kotagiri <sahithikotagiri24@gmail.com>
2024-08-28 18:36:04 +0530

    message commited

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+# why is it required? 
+# A folder is nothing but a package and it consists of several files, 
+# mcq generator is a local package and to treat it as a local package 
+# we need to create an init file as a convention from python side
